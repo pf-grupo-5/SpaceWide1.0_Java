3392d83b91225b5a8ed08206fc36ae2aecf1009e
SpaceWide <112873846+pf-grupo-5@users.noreply.github.com>
2022-10-05 10:11:52 -0300

    SpaceWide1.2.3

--- SpaceWide/src/java/classes/Usuario.java
+++ SpaceWide/src/java/classes/Usuario.java
@@ -8,96 +8,108 @@ import java.text.SimpleDateFormat;
  * @author aluno
  */
 public class Usuario {
-	private int id;
-	private String nome = "";
-	private String email = "";
-	private String senha = "";
-	private String estado = "";
-	private String acesso = "";
-	private String codigo_validador = "";
-	private Timestamp data_de_criacao;
-	private Timestamp data_da_ultima_modificacao;
 
-	public int getId() {
-		return id;
-	}
+    private int id;
+    private String nome = "";
+    private String email = "";
+    private String senha = "";
+    private String estado = "";
+    private String acesso = "";
+    private String localizacao_da_imagem_de_perfil = "";
+    private String nome_artistico = "";
+    private String codigo_validador = "";
+    private Timestamp data_de_criacao;
+    private Timestamp data_da_ultima_modificacao;
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public String getSenha() {
-		return senha;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
+    public String getSenha() {
+        return senha;
+    }
 
-	public String getEstado() {
-		return estado;
-	}
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 
-	public void setEstado(String estado) {
-		this.estado = estado;
-	}
+    public String getEstado() {
+        return estado;
+    }
 
-	public String getCodigo_validador() {
-		return codigo_validador;
-	}
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 
-	public void setCodigo_validador(String codigo_validador) {
-		this.codigo_validador = codigo_validador;
-	}
+    public String getCodigo_validador() {
+        return codigo_validador;
+    }
 
-	SimpleDateFormat dateFormat = new SimpleDateFormat("M/dd/yyyy");
+    public void setCodigo_validador(String codigo_validador) {
+        this.codigo_validador = codigo_validador;
+    }
 
-	public String getData_de_criacao() {
-		return String.format("%1$TD %1$TT", data_de_criacao);
-	}
+    SimpleDateFormat dateFormat = new SimpleDateFormat("M/dd/yyyy");
 
-	public void setData_de_criacao(Timestamp data_de_criacao) {
-		this.data_de_criacao = data_de_criacao;
-	}
+    public String getData_de_criacao() {
+        return String.format("%1$TD %1$TT", data_de_criacao);
+    }
 
-	public String getData_da_ultima_modificacao() {
-		return String.format("%1$TD %1$TT", data_da_ultima_modificacao);
-	}
+    public void setData_de_criacao(Timestamp data_de_criacao) {
+        this.data_de_criacao = data_de_criacao;
+    }
 
-	public void setData_da_ultima_modificacao(Timestamp data_da_ultima_modificacao) {
-		this.data_da_ultima_modificacao = data_da_ultima_modificacao;	
-	}
+    public String getData_da_ultima_modificacao() {
+        return String.format("%1$TD %1$TT", data_da_ultima_modificacao);
+    }
 
-	public String getAcesso() {
-		
-		return acesso;
-	}
+    public void setData_da_ultima_modificacao(Timestamp data_da_ultima_modificacao) {
+        this.data_da_ultima_modificacao = data_da_ultima_modificacao;
+    }
 
-	public void setAcesso(String acesso) {
-		this.acesso = acesso;
-	}
+    public String getAcesso() {
 
-	public SimpleDateFormat getDateFormat() {
-		return dateFormat;
-	}
+        return acesso;
+    }
 
-	public void setDateFormat(SimpleDateFormat dateFormat) {
-		this.dateFormat = dateFormat;
-	}
+    public void setAcesso(String acesso) {
+        this.acesso = acesso;
+    }
+
+    public SimpleDateFormat getDateFormat() {
+        return dateFormat;
+    }
+
+    public void setDateFormat(SimpleDateFormat dateFormat) {
+        this.dateFormat = dateFormat;
+    }
+    
+
+    public String getNome_artistico() {
+        return nome_artistico;
+    }
+
+    public void setNome_artistico(String nome_artistico) {
+        this.nome_artistico = nome_artistico;
+    }
 }
---
+++ SpaceWide/src/java/dao/UsuarioDao.java
@@ -0,0 +1,243 @@
+package dao;
+
+import classes.Usuario;
+import static dao.Dao.getConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsuarioDao {
+
+    public static Usuario getUsuarioById(int id) {
+        Usuario usuario = null;
+        try {
+            Connection con = getConnection();
+            PreparedStatement ps = (PreparedStatement) con.prepareStatement("select * from usuario where id=?");
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                usuario = new Usuario();
+                usuario.setId(rs.getInt("id"));
+                usuario.setNome(rs.getString("nome"));
+                usuario.setEmail(rs.getString("email"));
+                usuario.setSenha(rs.getString("senha"));
+                usuario.setAcesso(rs.getString("acesso"));
+            }
+        } catch (Exception erro) {
+            System.out.println(erro);
+        }
+        return usuario;
+    }
+
+    public static int editarUsuario(Usuario usuario) {
+        int status = 0;
+        try {
+            Connection con = getConnection();
+            PreparedStatement ps = (PreparedStatement) con.prepareStatement("UPDATE usuario SET nome=?, email=?, acesso=? WHERE id=?");
+            ps.setString(1, usuario.getNome());
+            ps.setString(2, usuario.getEmail());
+            ps.setString(3, usuario.getAcesso());
+            ps.setInt(4, usuario.getId());
+            status = ps.executeUpdate();
+        } catch (Exception erro) {
+            System.out.println(erro);
+        }
+        return status;
+    }
+
+    public static List<Usuario> getUsuarios(int inicio, int total) {
+        List<Usuario> list = new ArrayList<Usuario>();
+        try {
+            Connection con = getConnection();
+            PreparedStatement ps = (PreparedStatement) con.prepareStatement("SELECT * FROM usuario ORDER BY id LIMIT " + (inicio - 1) + " ," + total);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Usuario usuario = new Usuario();
+                usuario.setId(rs.getInt("id"));
+                usuario.setNome(rs.getString("nome"));
+                usuario.setEmail(rs.getString("email"));
+                usuario.setSenha(rs.getString("senha"));
+                usuario.setAcesso(rs.getString("acesso"));
+                usuario.setEstado(rs.getString("estado"));
+                list.add(usuario);
+            }
+        } catch (Exception erro) {
+            System.out.println(erro);
+        }
+        return list;
+    }
+    
+    public static List<Usuario> getUsuariosArtistas(int inicio, int total) {
+        List<Usuario> list = new ArrayList<Usuario>();
+        try {
+            Connection con = getConnection();
+            PreparedStatement ps = (PreparedStatement) con.prepareStatement("SELECT * FROM usuario ORDER BY id where acesso = 'artista' ");
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Usuario usuario = new Usuario();
+                usuario.setId(rs.getInt("id"));
+                usuario.setNome(rs.getString("nome"));
+                usuario.setEmail(rs.getString("email"));
+                usuario.setSenha(rs.getString("senha"));
+                usuario.setAcesso(rs.getString("acesso"));
+                usuario.setEstado(rs.getString("estado"));
+                list.add(usuario);
+            }
+        } catch (Exception erro) {
+            System.out.println(erro);
+        }
+        return list;
+    }
+    
+
+    public static List<Usuario> getRelatorio() {
+        List<Usuario> list = new ArrayList<Usuario>();
+        try {
+            Connection con = getConnection();
+            PreparedStatement ps = (PreparedStatement) con.prepareStatement("SELECT * FROM usuario");
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Usuario usuario = new Usuario();
+                usuario.setId(rs.getInt("id"));
+                usuario.setNome(rs.getString("nome"));
+                usuario.setEmail(rs.getString("email"));
+                usuario.setAcesso(rs.getString("acesso"));
+                list.add(usuario);
+            }
+        } catch (Exception erro) {
+            System.out.println(erro);
+        }
+        return list;
+    }
+
+    public static int getContagem() {
+        int contagem = 0;
+        try {
+            Connection con = getConnection();
+            PreparedStatement ps = (PreparedStatement) con.prepareStatement("SELECT count(*) AS contagem FROM usuario");
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                contagem = rs.getInt("contagem");
+            }
+        } catch (Exception erro) {
+            System.out.println(erro);
+        }
+        return contagem;
+    }
+
+    public static int[] getRelatorioUsuarios() {
+
+        int[] valores = {0, 0, 0};
+
+        try {
+            Connection con = getConnection();
+            PreparedStatement ps = (PreparedStatement) con.prepareStatement("SELECT count(*) AS administrador FROM usuario where acesso = 'administrador'");
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                valores[0] = rs.getInt("administrador");
+            }
+
+            ps = (PreparedStatement) con.prepareStatement("SELECT count(*) AS artista FROM usuario where acesso = 'artista'");
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                valores[1] = rs.getInt("artista");
+            }
+            ps = (PreparedStatement) con.prepareStatement("SELECT count(*) AS utente FROM usuario where acesso = 'utente'");
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                valores[2] = rs.getInt("utente");
+            }
+
+        } catch (Exception erro) {
+            System.out.println(erro);
+        }
+        return valores;
+    }
+
+    public static int excluirUsuario(Usuario usuario) {
+        int status = 0;
+        try {
+            Connection con = getConnection();
+            PreparedStatement ps = (PreparedStatement) con.prepareStatement("DELETE FROM usuario WHERE id=?");
+            ps.setInt(1, usuario.getId());
+            status = ps.executeUpdate();
+        } catch (Exception erro) {
+            System.out.println(erro);
+        }
+        return status;
+    }
+
+    public static int bloquearUsuario(Usuario usuario) {
+        int status = 0;
+        String statusdousuario;
+
+        if (usuario.getEstado().equals("ativo")) {
+            statusdousuario = "inativo";
+        } else {
+            statusdousuario = "ativo";
+        }
+        try {
+            Connection con = getConnection();
+            PreparedStatement ps = (PreparedStatement) con.prepareStatement("UPDATE usuario SET estado=? WHERE id=?");
+            ps.setString(1, statusdousuario);
+            ps.setInt(2, usuario.getId());
+            status = ps.executeUpdate();
+        } catch (Exception erro) {
+            System.out.println(erro);
+        }
+        return status;
+    }
+
+    public static int cadastrarUsuario(Usuario usuario) {
+        int status = 0;
+        try {
+            Connection con = getConnection();
+            PreparedStatement ps = (PreparedStatement) con.prepareStatement("INSERT INTO `usuario` (`nome`, `nome_artistico`, `email`, `senha`,`acesso`, `codigo_validador`, `estado`) VALUES(?,?,?,?,?,'ativo')");
+            ps.setString(1, usuario.getNome());
+            ps.setString(2, usuario.getEmail());
+            ps.setString(3, usuario.getSenha());
+            ps.setString(4, usuario.getAcesso());
+            status = ps.executeUpdate();
+        } catch (Exception erro) {
+            System.out.println(erro);
+        }
+        return status;
+    }
+
+    public static Usuario logar(String email, String senha) {
+        Usuario usuario = new Usuario();
+        try {
+            Connection con = getConnection();
+            PreparedStatement ps = (PreparedStatement) con.prepareStatement("select * from usuario where email=?");
+            ps.setString(1, email);
+            ResultSet rs = ps.executeQuery();
+            //Verifica se a consulta retornou resultado
+            if (rs.next()) {
+                if (rs.getString("status").equals("ativo")) {
+                    if (rs.getString("senha").equals(senha)) {
+                        usuario.setId(rs.getInt("id"));
+                        usuario.setNome(rs.getString("nome"));
+                        usuario.setEmail(rs.getString("email"));
+                        usuario.setSenha(rs.getString("senha"));
+                        usuario.setAcesso(rs.getString("acesso"));
+                    } else {
+                        //Senha errada
+                        usuario = null;
+                    }
+                } else {
+                    //Usuário Inativo
+                    usuario = null;
+                }
+            } else {
+                // E-mail não existe
+                usuario = null;
+            }
+        } catch (Exception erro) {
+            System.out.println(erro);
+        }
+        return usuario;
+    }
+
+}
